b50ce30526e178705277b54e931ddf0cb026fde0
pranav614 <pranavreddy614@gmail.com>
2024-01-15 16:44:59 +0530

    first commit

--- src/VideoContainer.js
+++ src/VideoContainer.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+
 
 const VideoContainer = () => {
-  const store=useSelector(store=> store)
-  console.log(store)
+
   return (
     <div>VideoContainer</div>
   )
